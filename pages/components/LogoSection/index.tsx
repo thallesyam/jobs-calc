06ce1e233cf47817fea2a6ae3fc45123fe851804
Thalles Ian <thallesyam@users.=.github.com>
2021-09-20 22:55:49 -0300

    feat: componentzation Logo section

---
+++ pages/components/LogoSection/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export function LoginSection() {
+  return (
+    <section className="h-screen bg-gray800 flex flex-col justify-center align-center p-5 text-center">
+      <div>
+        <Image src="/assets/icons/logo.svg" alt="Logo" width="220" height="48"/>
+
+        <h2 className="text-ice900 text-2xl font-semibold font-ibm leading-10 mx-auto mt-7">
+          O Jobscalc é o lugar perfeito para você <br />
+          que busca ter uma vida profissional <br />
+          mais organizada.
+        </h2>
+      </div> 
+    </section>
+  )
+}
